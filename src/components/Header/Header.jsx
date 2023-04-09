3f7e8fadb5ea68ae341793e346fd40e9acdbb080
Your Name <Rimon0000>
2023-04-09 22:44:30 +0600

    navbar done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import './Header.css'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <h2>From header</h2>
-        </div>
+
+        <nav className='header grid grid-cols-3'>
+            <h1 className='header-logo'>OpportunityHub</h1>
+            <div className='li-list'>
+               <Link to="/">Home</Link>
+               <Link to="/statistics">Statistics</Link>
+               <Link to="/applied">Applied Jobs</Link>
+               <Link to="/blog">Blog</Link>
+            </div>
+            <div>
+                <Link><button className='btn-applying'>Start Applying</button></Link>
+            </div>
+        </nav>
+
     );
 };
 
